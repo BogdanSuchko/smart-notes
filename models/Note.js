6adbbc87b23e44723903ae189d7c599845532027
BogdanSuchko <bahdan.suchko@yandex.by>
2024-12-02 15:02:06 +0300

    Adding new features

--- models/Note.js
+++ models/Note.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-const noteSchema = new mongoose.Schema({
+const NoteSchema = new mongoose.Schema({
 	title: String,
 	content: String,
 	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	tags: [String],
+	attachments: [{
+		filename: String,
+		data: Buffer,
+		contentType: String
+	}],
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', NoteSchema);
